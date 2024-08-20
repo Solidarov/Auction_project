f07fa238ea463c51a90bb411b761f6a896feb5e1
Solidarov <pavlo.nesteruk.work@gmail.com>
2024-08-20 14:11:49 +0300

    Add new model "AuctionLot" Register it in admin.py Run first migrations

--- auction_lot_app/models.py
+++ auction_lot_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AuctionLot(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    # write func that checks if end_price > start_price
+    end_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()  # write func that checks if end_date > start_date
+
+    # How to upload multiple images?
+    # image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
